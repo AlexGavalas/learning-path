f93a93fda50a4b7639ffedd30c04b14cbaa49adf
AlexGavalas <alex.gavalas.92@gmail.com>
2022-01-23 23:18:18 +0200

    feat: press / to focus search area

--- components/search-area.tsx
+++ components/search-area.tsx
@@ -1,4 +1,5 @@
 import type { Dispatch, FormEventHandler, SetStateAction } from 'react';
+import { useEffect, useRef } from 'react';
 
 import type { Post } from '../lib/posts';
 
@@ -11,6 +12,23 @@ export const SearchArea = ({
     posts: Post[];
     setPosts: Dispatch<SetStateAction<Post[]>>;
 }) => {
+    const queryEl = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === '/') {
+                e.stopImmediatePropagation();
+                queryEl.current?.focus();
+            }
+        };
+
+        document.addEventListener('keyup', handler);
+
+        return () => {
+            document.removeEventListener('keyup', handler);
+        };
+    }, []);
+
     const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
 
@@ -33,9 +51,10 @@ export const SearchArea = ({
         <form onSubmit={onSubmit} className="relative h-8 flex items-center">
             <input
                 name={QUERY_FIELD_NAME}
+                ref={queryEl}
                 className="pl-2 h-full w-full border-[1px] dark:border-0 border-solid bg-transparent dark:bg-gray-800 rounded dark:placeholder-gray-300 dark:placeholder-opacity-50"
                 autoComplete="off"
-                placeholder="What are you looking for?"
+                placeholder="Search notes"
             />
             <button className="cursor-pointer h-6 absolute right-1 bg-transparent rounded border-0 text-black dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-800">
                 Search
