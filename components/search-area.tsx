2f63c14e78991a611573198909797c34986b5c03
AlexGavalas <alex.gavalas.92@gmail.com>
2022-01-24 22:54:58 +0200

    feat: added clear search button

--- components/search-area.tsx
+++ components/search-area.tsx
@@ -1,5 +1,10 @@
-import type { Dispatch, FormEventHandler, SetStateAction } from 'react';
-import { useEffect, useRef } from 'react';
+import type {
+    Dispatch,
+    FormEventHandler,
+    MouseEventHandler,
+    SetStateAction,
+} from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import type { Post } from '../lib/posts';
 
@@ -13,6 +18,7 @@ export const SearchArea = ({
     setPosts: Dispatch<SetStateAction<Post[]>>;
 }) => {
     const queryEl = useRef<HTMLInputElement>(null);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
         const handler = (e: KeyboardEvent) => {
@@ -32,19 +38,22 @@ export const SearchArea = ({
     const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
 
-        const formData = new FormData(e.currentTarget);
-
-        const userQuery = formData.get(QUERY_FIELD_NAME);
-
-        if (!userQuery) {
+        if (!query) {
             return setPosts(posts);
         }
 
-        const query = new RegExp(userQuery.toString(), 'i');
+        const userQuery = new RegExp(query, 'i');
 
-        const filteredPosts = posts.filter(({ title }) => query.test(title));
+        const filteredPosts = posts.filter(({ title }) =>
+            userQuery.test(title)
+        );
 
         setPosts(filteredPosts);
+    };
+
+    const onClear: MouseEventHandler<HTMLButtonElement> = () => {
+        setQuery('');
+        setPosts(posts);
     };
 
     return (
@@ -52,13 +61,26 @@ export const SearchArea = ({
             <input
                 name={QUERY_FIELD_NAME}
                 ref={queryEl}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
                 className="pl-2 h-full w-full border-[1px] dark:border-0 border-solid bg-transparent dark:bg-neutral-800 rounded dark:placeholder-gray-300 dark:placeholder-opacity-50"
                 autoComplete="off"
                 placeholder="Search notes"
             />
-            <button className="cursor-pointer h-6 absolute right-1 bg-transparent rounded border-0 text-black dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600">
-                Search
-            </button>
+            <div className="h-full p-1 absolute right-0 flex gap-2">
+                {query && (
+                    <button
+                        className="cursor-pointer rounded border-0 text-black dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600"
+                        onClick={onClear}
+                        type="button"
+                    >
+                        Clear
+                    </button>
+                )}
+                <button className="cursor-pointer rounded border-0 text-black dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600">
+                    Search
+                </button>
+            </div>
         </form>
     );
 };
