bcf8f1171117207f4679a67778d04d83c3baea19
AlexGavalas <alex.gavalas.92@gmail.com>
2022-02-20 21:28:56 +0200

    feat: added auth

---
+++ lib/use-user.tsx
@@ -0,0 +1,66 @@
+import { FC, useEffect, useState, createContext, useContext } from 'react';
+import { supabase } from './supabase';
+import { Session, User, AuthChangeEvent } from '@supabase/supabase-js';
+
+type UserContextType = {
+    user: User | null;
+    userLoaded: boolean;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+    undefined
+);
+
+const handleAuthChange = async (
+    event: AuthChangeEvent,
+    session: Session | null
+) => {
+    await fetch('/api/auth', {
+        body: JSON.stringify({ event, session }),
+        credentials: 'same-origin',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        method: 'POST',
+    });
+};
+
+export const UserContextProvider: FC = ({ children }) => {
+    const [userLoaded, setUserLoaded] = useState(true);
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const session = supabase.auth.session();
+
+        setUser(session?.user ?? null);
+        setUserLoaded(true);
+
+        const { data: authListener } = supabase.auth.onAuthStateChange(
+            (event, session) => {
+                handleAuthChange(event, session);
+                setUser(session?.user ?? null);
+            }
+        );
+
+        return () => {
+            authListener?.unsubscribe();
+        };
+    }, []);
+
+    const value = {
+        user,
+        userLoaded,
+    };
+
+    return (
+        <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    );
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+
+    if (context === undefined) {
+        throw new Error(`useUser must be used within a UserContextProvider.`);
+    }
+
+    return context;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,27 @@ import { ThemeProvider } from 'next-themes';
 
 import '../styles/global.css';
 
+import { UserContextProvider } from '../lib/use-user';
 import { Header } from '../components/header';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <ThemeProvider attribute="class">
             <div className="flex flex-col m-auto max-w-xl">
-                <Head>
-                    <link rel="icon" href="/favicon.ico" />
-                    <title>Learning Path</title>
-                </Head>
-                <Header />
-                <AnimatePresence
-                    exitBeforeEnter
-                    initial={false}
-                    onExitComplete={() => window.scrollTo(0, 0)}
-                >
-                    <Component {...pageProps} />
-                </AnimatePresence>
+                <UserContextProvider>
+                    <Head>
+                        <link rel="icon" href="/favicon.ico" />
+                        <title>Learning Path</title>
+                    </Head>
+                    <Header />
+                    <AnimatePresence
+                        exitBeforeEnter
+                        initial={false}
+                        onExitComplete={() => window.scrollTo(0, 0)}
+                    >
+                        <Component {...pageProps} />
+                    </AnimatePresence>
+                </UserContextProvider>
             </div>
         </ThemeProvider>
     );
