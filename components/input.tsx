ea1e5b81bed3a11e919b13bac0db3bbf8a20565c
AlexGavalas <alex.gavalas.92@gmail.com>
2022-02-21 20:23:17 +0200

    refactor: input

--- components/input.tsx
+++ components/input.tsx
@@ -1,12 +1,18 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes, ForwardedRef } from 'react';
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = (props: InputProps) => {
+const InputComponent = (
+    props: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+) => {
     return (
         <input
             {...props}
+            ref={ref}
             className="pl-2 w-full h-full border-[1px] dark:border-0 border-solid bg-transparent dark:bg-neutral-800 rounded dark:placeholder-gray-300 dark:placeholder-opacity-50"
         />
     );
 };
+
+export const Input = forwardRef(InputComponent);
