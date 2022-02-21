8fd21680315bd799ae8a94f412b5ae27e014699a
AlexGavalas <alex.gavalas.92@gmail.com>
2022-02-21 20:24:52 +0200

    feat: added post delete functionality

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,9 +1,12 @@
 import type { NextPage, GetServerSideProps } from 'next';
-import { useState, FormEventHandler, useRef } from 'react';
+import { useState, useCallback, FormEventHandler, useRef } from 'react';
 import { intervalToDuration, add, format } from 'date-fns';
 
 import Layout from '../components/layout';
-import { Input } from '../components/input';
+import { Dialog } from '../components/dialog';
+import { Textarea } from '../components/textarea';
+import { Button } from '../components/button';
+import { Loader } from '../components/loader';
 import { supabase } from '../lib/supabase';
 import { redis } from '../lib/redis';
 import { useUser } from '../lib/use-user';
@@ -60,38 +63,73 @@ const EXPIRE_AFTER_SECS = 60 * 60 * 24 * 5;
 const Feed: NextPage<FeedProps> = ({ posts: initialPosts, isLoggedIn }) => {
     const [posts, setPosts] = useState<Post[]>(initialPosts);
     const [hasUser, setHasUser] = useState(isLoggedIn);
+    const [submitting, setSubmitting] = useState(false);
+    const [editingPost, setEditingPost] = useState<Post | null>(null);
+    const [openDialog, setOpenDialog] = useState(false);
     const formRef = useRef<HTMLFormElement>(null);
+    const keyToDelete = useRef<string>();
 
     const { user, userLoaded } = useUser();
 
+    const closeDialog = useCallback(() => setOpenDialog(false), []);
+
     const addNewNote: FormEventHandler<HTMLFormElement> = async (e) => {
-        e.preventDefault();
+        try {
+            setSubmitting(true);
 
-        if (!formRef.current) return;
+            e.preventDefault();
 
-        const user = supabase.auth.user();
+            if (!formRef.current) return;
 
-        const post = new FormData(formRef.current).get('post');
+            const user = supabase.auth.user();
 
-        if (!post || !user) return;
+            const post = new FormData(formRef.current).get('post');
 
-        const key = `posts:${Date.now()}`;
+            if (!post || !user) return;
 
-        const data = {
-            post: post.toString().slice(0, MAX_CHARS),
-            name: user.user_metadata.name,
-        };
+            const key = `posts:${Date.now()}`;
 
-        await redis.set(key, JSON.stringify(data));
-        await redis.expire(key, EXPIRE_AFTER_SECS);
+            const data = {
+                post: post.toString().slice(0, MAX_CHARS),
+                name: user.user_metadata.name,
+            };
 
-        formRef.current.reset();
+            const { error } = await redis.set(key, JSON.stringify(data));
 
-        // Optimistic state update
-        setPosts((prev) => [{ data, key }, ...prev]);
+            if (error) return;
+
+            await redis.expire(key, EXPIRE_AFTER_SECS);
+
+            formRef.current.reset();
+
+            // Optimistic state update
+            setPosts((prev) => [{ data, key }, ...prev]);
+        } catch (e) {
+        } finally {
+            setSubmitting(false);
+        }
     };
 
     const userExists = Boolean(userLoaded && (user || hasUser));
+
+    const handleDelete = async () => {
+        const key = keyToDelete.current;
+
+        if (!key) return;
+
+        const { error } = await redis.del(key);
+
+        if (error) {
+            // TODO: Show an error to the user
+            return;
+        }
+
+        setPosts((prev) => prev.filter((item) => item.key !== key));
+    };
+
+    const handleEdit = (post: Post) => {
+        setEditingPost(post);
+    };
 
     return (
         <Layout>
@@ -112,28 +150,27 @@ const Feed: NextPage<FeedProps> = ({ posts: initialPosts, isLoggedIn }) => {
                 </p>
                 {userExists ? (
                     <form
-                        className="relative h-8 flex"
+                        className="relative flex flex-col"
                         onSubmit={addNewNote}
                         ref={formRef}
                     >
-                        <Input
+                        <Textarea
                             name="post"
                             placeholder={`Post anything (max ${MAX_CHARS} characters)`}
-                            autoComplete="off"
                         />
-                        <div className="h-full p-1 absolute right-0 flex gap-2">
-                            <button className="rounded border-0 text-black dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600">
-                                Add
-                            </button>
-                            <button
+                        {submitting && <Loader />}
+                        <div className="h-8 py-1 flex gap-2 justify-between">
+                            <Button
                                 type="button"
+                                variant="danger"
                                 onClick={() => {
                                     setHasUser(false);
                                     supabase.auth.signOut();
                                 }}
                             >
                                 Sign out
-                            </button>
+                            </Button>
+                            <Button>Post it</Button>
                         </div>
                     </form>
                 ) : (
@@ -141,8 +178,7 @@ const Feed: NextPage<FeedProps> = ({ posts: initialPosts, isLoggedIn }) => {
                         <p className="m-0 text-black dark:text-white">
                             Login to be able to post
                         </p>
-                        <button
-                            className="h-8 rounded border-0 text-black dark:text-white hover:bg-gray-300 bg-gray-200 dark:bg-gray-500 dark:hover:bg-gray-600"
+                        <Button
                             onClick={() => {
                                 supabase.auth.signIn(
                                     { provider: 'google' },
@@ -153,36 +189,82 @@ const Feed: NextPage<FeedProps> = ({ posts: initialPosts, isLoggedIn }) => {
                             }}
                         >
                             Login with Google
-                        </button>
+                        </Button>
                     </div>
                 )}
                 <ul className="mt-4">
-                    {posts.map(({ data: { post, name }, key }) => {
-                        const [, timestamp] = key.split(':');
+                    {posts.length > 0 ? (
+                        posts.map((post) => {
+                            const {
+                                data: { post: postData, name },
+                                key,
+                            } = post;
 
-                        const postDate = format(
-                            new Date(+timestamp),
-                            'dd/MM/yyyy HH:mm'
-                        );
+                            const [, timestamp] = key.split(':');
 
-                        return (
-                            <li
-                                key={key}
-                                className="list-none py-6 border-0 border-solid last:border-b-0"
-                            >
-                                <figure className="flex flex-col">
-                                    <blockquote className="border-solid border-0 border-l-2 border-l-[#4675aa] pl-4">
-                                        <p>{post}</p>
-                                    </blockquote>
-                                    <figcaption className="self-end italic">
-                                        - {name} at {postDate}
-                                    </figcaption>
-                                </figure>
-                            </li>
-                        );
-                    })}
+                            const postDate = format(
+                                new Date(+timestamp),
+                                'dd/MM/yyyy HH:mm'
+                            );
+
+                            const isMine = name === user?.user_metadata.name;
+
+                            return (
+                                <li
+                                    key={key}
+                                    className="list-none py-6 border-0 border-solid last:border-b-0 flex flex-col"
+                                >
+                                    <p className="group border-solid border-0 border-l-2 border-l-[#4675aa] pl-4">
+                                        {postData}
+                                    </p>
+                                    <div className="self-end flex gap-4 items-center">
+                                        {isMine && (
+                                            <div className="flex gap-2 h-5">
+                                                <Button
+                                                    variant="danger"
+                                                    onClick={() => {
+                                                        setOpenDialog(true);
+                                                        keyToDelete.current =
+                                                            key;
+                                                    }}
+                                                >
+                                                    Delete
+                                                </Button>
+                                                {/* <Button
+                                                    onClick={() => {
+                                                        handleEdit(post);
+                                                    }}
+                                                >
+                                                    Edit
+                                                </Button> */}
+                                            </div>
+                                        )}
+                                        <p className="italic text-sm ">
+                                            - {name} at {postDate}
+                                        </p>
+                                    </div>
+                                </li>
+                            );
+                        })
+                    ) : (
+                        <p className="text-center">No posts yet</p>
+                    )}
                 </ul>
             </section>
+            <Dialog open={openDialog} onClickOutside={closeDialog}>
+                <p>Are you sure you want to delete?</p>
+                <div className="flex gap-2 justify-end">
+                    <Button onClick={closeDialog}>No</Button>
+                    <Button
+                        onClick={() => {
+                            handleDelete();
+                            closeDialog();
+                        }}
+                    >
+                        Yes
+                    </Button>
+                </div>
+            </Dialog>
         </Layout>
     );
 };
