e42cef49d0e61b5c0f13ee744560def3c4568f4d
AlexGavalas <alex.gavalas.92@gmail.com>
2022-02-21 20:23:26 +0200

    refactor: loader

---
+++ components/loader.tsx
@@ -0,0 +1,3 @@
+export const Loader = () => (
+    <div className="animate-loader bg-no-repeat bg-center bg-gradient-to-r from-white via-[#4675aa] to-white bg-[length:25%_100%] w-full h-[2px] absolute bottom-[-8px]" />
+);
