31fdb12b0d13f8e0c6d275fd87fcd0fbd6a4db33
AlexGavalas <alex.gavalas.92@gmail.com>
2022-03-13 19:58:14 +0200

    fix: fauna dev mode

--- lib/fauna.ts
+++ lib/fauna.ts
@@ -1,6 +1,8 @@
 import faunadb from 'faunadb';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export const fauna = new faunadb.Client({
     secret: process.env.NEXT_PUBLIC_FAUNA_SECRET,
-    // domain: 'db.eu.fauna.com',
+    ...(isDev && { domain: 'db.eu.fauna.com' }),
 });
