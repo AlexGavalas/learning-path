7a069ab061249f43cca4f48a7eda7a09bb7e5ecb
AlexGavalas <alex.gavalas.92@gmail.com>
2022-03-13 19:47:39 +0200

    test: new fauna region

--- lib/fauna.ts
+++ lib/fauna.ts
@@ -2,5 +2,5 @@ import faunadb from 'faunadb';
 
 export const fauna = new faunadb.Client({
     secret: process.env.NEXT_PUBLIC_FAUNA_SECRET,
-    domain: 'db.eu.fauna.com',
+    // domain: 'db.eu.fauna.com',
 });
