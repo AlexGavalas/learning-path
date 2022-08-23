675b8e18f9f7aaab498ff987e829ad7b64760878
AlexGavalas <alex.gavalas.92@gmail.com>
2022-08-23 23:04:14 +0300

    fix: mobile styles

--- components/header.tsx
+++ components/header.tsx
@@ -29,29 +29,27 @@ export const Header = () => {
               };
 
     return (
-        <>
-            <header className="flex justify-between w-full items-center border-0 border-solid border-b dark:border-b-white">
-                <div className="flex">
-                    <Link href="/">
-                        <a className="cursor-pointer hover:no-underline font-[700] p-2 m-2 text-3xl text-black dark:text-white rounded-lg hover:bg-gray-300 dark:hover:bg-gray-800">
-                            Learning Path
-                        </a>
-                    </Link>
+        <header className="flex justify-between w-full items-center border-0 border-solid border-b dark:border-b-white">
+            <div className="flex">
+                <Link href="/">
+                    <a className="cursor-pointer hover:no-underline font-[700] p-2 m-2 text-3xl text-black dark:text-white rounded-lg hover:bg-gray-300 dark:hover:bg-gray-800">
+                        Learning Path
+                    </a>
+                </Link>
+            </div>
+            {mounted && (
+                <div className="mr-3">
+                    <Button onClick={toggleTheme} variant="wrapper">
+                        <Image
+                            src={image.src}
+                            alt={image.alt}
+                            width={20}
+                            height={20}
+                        />
+                    </Button>
                 </div>
-                {mounted && (
-                    <div className="mr-3">
-                        <Button onClick={toggleTheme} variant="wrapper">
-                            <Image
-                                src={image.src}
-                                alt={image.alt}
-                                width={20}
-                                height={20}
-                            />
-                        </Button>
-                    </div>
-                )}
-            </header>
-        </>
+            )}
+        </header>
     );
 };
 
