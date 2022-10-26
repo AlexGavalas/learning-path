284b2fd2758e31b4eb44d13e6bea16bed5d5215f
Alex Gavalas <alex.gavalas@workable.com>
2022-10-26 10:10:38 +0300

    feat: added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from 'next-themes';
 
@@ -8,10 +9,12 @@ import '../styles/global.css';
 import { UserContextProvider } from '@lib/use-user';
 import { Header } from '@components/header';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <ThemeProvider attribute="class">
-            <div className="flex flex-col m-auto max-w-xl">
+        <>
+            <ThemeProvider attribute="class">
                 <UserContextProvider>
                     <Head>
                         <title>Learning Path</title>
@@ -20,17 +23,20 @@ const App = ({ Component, pageProps }: AppProps) => {
                             content="width=device-width, initial-scale=1"
                         />
                     </Head>
-                    <Header />
-                    <AnimatePresence
-                        mode="wait"
-                        initial={false}
-                        onExitComplete={() => window.scrollTo(0, 0)}
-                    >
-                        <Component {...pageProps} />
-                    </AnimatePresence>
+                    <div className="flex flex-col m-auto max-w-xl">
+                        <Header />
+                        <AnimatePresence
+                            mode="wait"
+                            initial={false}
+                            onExitComplete={() => window.scrollTo(0, 0)}
+                        >
+                            <Component {...pageProps} />
+                        </AnimatePresence>
+                    </div>
                 </UserContextProvider>
-            </div>
-        </ThemeProvider>
+            </ThemeProvider>
+            {isProd && <Analytics />}
+        </>
     );
 };
 
