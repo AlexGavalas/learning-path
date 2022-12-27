32fb6326ca3f1c5059f4189d80d008e1a2e7c8d3
Alex Gavalas <alex.gavalas@workable.com>
2022-12-27 13:00:35 +0200

    edge config test

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
-    },
     experimental: {
         scrollRestoration: true,
+        runtime: 'experimental-edge',
     },
 };
 
