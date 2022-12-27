3a819922694bf09254cd9f8c40035852c337871e
Alex Gavalas <alex.gavalas@workable.com>
2022-12-27 11:59:45 +0200

    test: removes getserversideprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Layout } from '~components/layout';
 import { Banner } from '~features/banner';
 import { NotesList } from '~features/notes-list';
 import { SearchArea } from '~features/search-area';
-import { supabase } from '~lib/supabase';
+// import { supabase } from '~lib/supabase';
 
 import { type Note } from '../../types/notes.types';
 
@@ -20,56 +20,56 @@ export const config: PageConfig = {
     runtime: 'experimental-edge',
 };
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async ({
-    query,
-}) => {
-    const { data } = await supabase.rpc('get_notes_meta');
+// export const getServerSideProps: GetServerSideProps<HomeProps> = async ({
+//     query,
+// }) => {
+//     const { data } = await supabase.rpc('get_notes_meta');
 
-    const allNotes = data ?? [];
+//     const allNotes = data ?? [];
 
-    const q = query.q?.toString();
+//     const q = query.q?.toString();
 
-    if (q) {
-        const { data, error } = await supabase.rpc('search_notes', { q });
+//     if (q) {
+//         const { data, error } = await supabase.rpc('search_notes', { q });
 
-        if (error) {
-            return {
-                props: {
-                    notes: [],
-                    lines: {},
-                    error: true,
-                },
-            };
-        }
+//         if (error) {
+//             return {
+//                 props: {
+//                     notes: [],
+//                     lines: {},
+//                     error: true,
+//                 },
+//             };
+//         }
 
-        const lines = data.reduce<Lines>((acc, { title, line }) => {
-            acc[title] = (acc[title] || []).concat(line);
-            return acc;
-        }, {});
+//         const lines = data.reduce<Lines>((acc, { title, line }) => {
+//             acc[title] = (acc[title] || []).concat(line);
+//             return acc;
+//         }, {});
 
-        const noteTitles = new Set(data.map(({ title }) => title));
+//         const noteTitles = new Set(data.map(({ title }) => title));
 
-        const filteredNotes = allNotes.filter(({ title }) =>
-            noteTitles.has(title),
-        );
+//         const filteredNotes = allNotes.filter(({ title }) =>
+//             noteTitles.has(title),
+//         );
 
-        return {
-            props: {
-                notes: filteredNotes,
-                lines,
-            },
-        };
-    }
+//         return {
+//             props: {
+//                 notes: filteredNotes,
+//                 lines,
+//             },
+//         };
+//     }
 
-    return {
-        props: {
-            notes: allNotes,
-            lines: {},
-        },
-    };
-};
+//     return {
+//         props: {
+//             notes: allNotes,
+//             lines: {},
+//         },
+//     };
+// };
 
-const Home: NextPage<HomeProps> = ({ notes, lines }) => {
+const Home: NextPage<HomeProps> = ({ notes = [], lines = {} }) => {
     return (
         <Layout>
             <section className="text-xl leading-8">
