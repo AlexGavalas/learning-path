617e14799a28dade46099548da4efa79db4a9637
Alex Gavalas <alex.gavalas@workable.com>
2022-12-28 19:47:19 +0200

    test: adds locale on next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
     experimental: {
         scrollRestoration: true,
     },
