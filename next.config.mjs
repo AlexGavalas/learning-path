52e2dd1bf77defa5f81f768320d4c7a9d8c94705
Alex Gavalas <alex.gavalas@workable.com>
2022-12-28 20:26:43 +0200

    testing html lang

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+        localeDetection: false,
+    },
     experimental: {
         scrollRestoration: true,
     },
