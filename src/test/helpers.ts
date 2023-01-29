2b4e713e22cdb5baf55f5fde98563ff2ea54fd43
Alex Gavalas <alex.gavalas@workable.com>
2023-01-29 21:56:11 +0100

    refactor: simplifies test helpers types

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -1,23 +1,17 @@
-import {
-    type RenderHookOptions,
-    type RenderOptions,
-    render,
-    renderHook,
-} from '@testing-library/react';
+import { render, renderHook } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { type ReactElement } from 'react';
 
 export * from '@testing-library/react';
 
-export const renderWithUser = (ui: ReactElement, options?: RenderOptions) => ({
+type RenderParams = Parameters<typeof render>;
+type RenderHookParams = Parameters<typeof renderHook>;
+
+export const renderWithUser = (...props: RenderParams) => ({
     user: userEvent.setup(),
-    ...render(ui, options),
+    ...render(...props),
 });
 
-export const renderHookWithUser = <Result>(
-    render: (p: unknown) => Result,
-    options?: RenderHookOptions<Result>,
-) => ({
+export const renderHookWithUser = (...props: RenderHookParams) => ({
     user: userEvent.setup(),
-    ...renderHook(render, options),
+    ...renderHook(...props),
 });
