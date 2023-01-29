26bf2478f53a75ec9d8db13e780b4842fa3128c6
Alex Gavalas <alex.gavalas@workable.com>
2023-01-29 20:46:04 +0100

    feat: adds custom commit type enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
+const { rules } = require('@commitlint/config-conventional');
+
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', [...rules['type-enum'][2], 'content']],
+    },
 };
