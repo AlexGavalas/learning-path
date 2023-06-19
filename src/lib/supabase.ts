117f119fb2a3ae72c66af681af81b39687c575d7
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-06-19 22:50:52 +0300

    refactor: updates supabase api lib types

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-import { type Database } from '~types/database.types';
+import type { Database } from '~types/database.types';
 
 export const supabase = createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.PUBLIC_SUPABASE_URL ?? import.meta.env.PUBLIC_SUPABASE_URL,
+    process.env.PUBLIC_SUPABASE_ANON_KEY ??
+        import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
 );
