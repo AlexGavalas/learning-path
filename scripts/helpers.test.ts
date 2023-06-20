8c773d92b9f704ac18c736902e6f3faa62855246
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-06-20 23:33:18 +0300

    test: adds tests for script helpers

---
+++ scripts/helpers.test.ts
@@ -0,0 +1,103 @@
+import fs from 'fs/promises';
+
+import { getEnvVariable, readFile, toISOString, writeFile } from './helpers';
+
+jest.mock('fs/promises');
+
+describe('getEnvVariable', () => {
+    const envVar = 'TEST_ENV_VAR';
+
+    afterEach(() => {
+        process.env[envVar] = undefined;
+    });
+
+    describe('when the environment variable is defined', () => {
+        const value = 'test value';
+
+        beforeAll(() => {
+            process.env[envVar] = value;
+        });
+
+        it('returns its value', () => {
+            expect(getEnvVariable(envVar)).toEqual(value);
+        });
+    });
+
+    describe('when the environment variable is "undefined"', () => {
+        beforeAll(() => {
+            process.env[envVar] = 'undefined';
+        });
+
+        it('throws an error', () => {
+            expect(() => getEnvVariable(envVar)).toThrowError(
+                `${envVar} is not defined in env.`,
+            );
+        });
+    });
+
+    describe('when the environment variable is "null"', () => {
+        const envVar = 'TEST_ENV_VAR';
+
+        beforeAll(() => {
+            process.env[envVar] = 'null';
+        });
+
+        it('throws an error', () => {
+            expect(() => getEnvVariable(envVar)).toThrowError(
+                `${envVar} is not defined in env.`,
+            );
+        });
+    });
+
+    describe('when the environment variable is not defined', () => {
+        beforeAll(() => {
+            process.env[envVar] = undefined;
+        });
+
+        it('throws an error', () => {
+            expect(() => getEnvVariable(envVar)).toThrowError(
+                `${envVar} is not defined in env.`,
+            );
+        });
+    });
+});
+
+describe('toISOString', () => {
+    describe('when the date is in the format yyyy-MM-dd', () => {
+        const date = '2020-01-01';
+
+        it('returns the date in ISO format', () => {
+            expect(toISOString(date)).toBe('2020-01-01T00:00:00.000Z');
+        });
+    });
+
+    describe('when the date is not in the format yyyy-MM-dd', () => {
+        const date = '01-01-2020';
+
+        it('throws an error', () => {
+            expect(() => toISOString(date)).toThrowError('Invalid time value');
+        });
+    });
+});
+
+describe('readFile', () => {
+    it('calls readFile from fs/promises with the correct arguments', async () => {
+        await readFile('test-file');
+
+        expect(fs.readFile).toHaveBeenCalledTimes(1);
+        expect(fs.readFile).toHaveBeenCalledWith('test-file', 'utf8');
+    });
+});
+
+describe('writeFile', () => {
+    it('calls writeFile from fs/promises with the correct arguments', async () => {
+        await writeFile('test-file', 'contents');
+
+        expect(fs.writeFile).toHaveBeenCalledTimes(1);
+        expect(fs.writeFile).toHaveBeenCalledWith(
+            'test-file',
+            'contents',
+            'utf8',
+        );
+    });
+});
--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,22 +1,24 @@
 import { parse } from 'date-fns';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 export const toISOString = (date: string): string =>
     parse(date, 'yyyy-MM-dd', new Date()).toISOString();
 
 export const readFile = async (file: string): Promise<string> =>
-    await fs.promises.readFile(file, 'utf8');
+    await fs.readFile(file, 'utf8');
 
 export const writeFile = async (file: string, data: string): Promise<void> => {
-    await fs.promises.writeFile(file, data, 'utf8');
+    await fs.writeFile(file, data, 'utf8');
 };
 
 export const getEnvVariable = (envVar: string): string => {
     const value = process.env[envVar];
 
-    if (typeof value !== 'string') {
-        console.log(`${envVar} is not defined in env.`);
-        process.exit(1);
+    const valueExists =
+        typeof value === 'string' && value !== 'undefined' && value !== 'null';
+
+    if (!valueExists) {
+        throw new Error(`${envVar} is not defined in env.`);
     }
 
     return value;
