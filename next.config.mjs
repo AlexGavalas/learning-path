02204ff380f389f18241d14e0c67fa94088b5036
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-06-22 22:39:32 +0300

    chore: updates dependencies

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        scrollRestoration: true,
-    },
-    poweredByHeader: false,
-};
-
-export default nextConfig;
