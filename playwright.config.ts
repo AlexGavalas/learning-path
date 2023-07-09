c51e640d16f5001abb008902f4962b3a703fbf88
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-07-09 13:48:11 +0300

    Run e2e tests on preview deployments (#240)
    * refactor: runs e2e on preview deployments
    
    * fix: updates ci env config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const envVarBaseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL;
+const isCI = process.env.CI === 'true';
 
 const baseURL =
     typeof envVarBaseUrl === 'string' ? envVarBaseUrl : 'http://localhost:3000';
@@ -9,10 +10,12 @@ const config: PlaywrightTestConfig = {
     use: {
         baseURL,
     },
-    webServer: {
-        command: 'pnpm dev',
-        url: 'http://localhost:3000',
-    },
+    ...(!isCI && {
+        webServer: {
+            command: 'pnpm dev',
+            url: 'http://localhost:3000',
+        },
+    }),
 };
 
 export default config;
