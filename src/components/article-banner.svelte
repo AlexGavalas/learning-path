a3c5ba17a8761662d52d0761868eb95e3798c06e
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-08-03 22:55:17 +0300

    feat: improves article banner animation

--- src/components/article-banner.svelte
+++ src/components/article-banner.svelte
@@ -1,5 +1,5 @@
 <script>
-    const CELLS_NUM = 100;
+    const CELLS_NUM = 125;
     const ROWS = 5;
 </script>
 
@@ -8,38 +8,22 @@
     style="--cells: {CELLS_NUM}; --columns: {CELLS_NUM / ROWS}"
 >
     {#each Array(CELLS_NUM) as _, idx}
-        <div class="item" style="--idx: {idx}"></div>
+        <div
+            class="item bg-light-primary dark:bg-dark-primary"
+            style="--idx: {idx}"
+        ></div>
     {/each}
 </div>
 
 <style>
-    @property --t {
-        syntax: '<number>';
-        initial-value: 0;
-        inherits: true;
-    }
-
-    @property --counter {
-        syntax: '<number>';
-        initial-value: 0;
-        inherits: true;
-    }
-
-    @keyframes time {
+    @keyframes blink {
+        0%,
         100% {
-            --t: 30;
+            opacity: 0.75;
         }
-    }
-
-    @keyframes incrementCounter {
-        100% {
-            --counter: 40;
+        50% {
+            opacity: 0.25;
         }
-    }
-
-    :root {
-        --t: 0;
-        --counter: 0;
     }
 
     .wrapper {
@@ -48,39 +32,16 @@
 
         display: grid;
         grid-template-columns: repeat(var(--columns), 1fr);
-        gap: 10px;
+        gap: 0.3rem;
     }
 
     .item {
         aspect-ratio: 1;
-        border-radius: 5px;
-
-        animation:
-            30s time infinite linear,
-            1000s incrementCounter infinite linear;
+        border-radius: 20%;
+        animation: 3s blink infinite alternate ease-in-out;
 
         animation-delay: calc(
-            -1 * var(--total-stagger) + var(--idx) * var(--item-stagger) - 25s
-        );
-
-        animation-play-state: paused;
-
-        --x: calc(0.5 + 0.5 * sin(4.284 * var(--counter)));
-        --y: calc(0.5 + 0.5 * sin(7.284 * var(--counter)));
-        --z: calc(0.5 + 0.5 * sin(4 * var(--counter) + 2 * var(--t)));
-        --a: calc(
-            0.5 + 0.5 *
-                sin(
-                    (0.2 * cos(var(--t) / 100) + 0.8) * 49.123 * var(--counter) +
-                        var(--t)
-                )
-        );
-
-        background: rgba(
-            calc(100 + 90 * var(--x)),
-            calc(100 + 90 * var(--y)),
-            calc(100 + 90 * var(--z)),
-            calc(0.3 + var(--a))
+            -1 * var(--total-stagger) + var(--idx) * var(--item-stagger)
         );
     }
 </style>
