d80208657eacee407a62bfa75eda158cc7cb1234
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-11-13 01:36:28 +0200

    refactor: uses custom element from search area

--- src/components/loader.svelte
+++
@@ -1,3 +0,0 @@
-<div
-    class="absolute bottom-[-8px] h-[2px] w-full animate-loader bg-gradient-to-r from-white from-0% via-light-primary via-50% to-white to-100% bg-[length:25%_100%] bg-center bg-no-repeat dark:via-dark-primary"
-/>
