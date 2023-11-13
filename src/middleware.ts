50960bde068214d1bd26702262750205a36a0697
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-11-14 01:18:06 +0200

    refactor: makes middleware handler sync

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,22 @@ import { defineMiddleware } from 'astro:middleware';
 
 import { DEFAULT_THEME, THEME } from '~constants';
 
-export const onRequest = defineMiddleware(
-    async ({ request, cookies, url }, next) => {
-        const shouldToggleTheme =
-            request.method === 'GET' &&
-            url.searchParams.get('theme') === 'toggle';
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+export const onRequest = defineMiddleware(({ request, cookies, url }, next) => {
+    const shouldToggleTheme =
+        request.method === 'GET' && url.searchParams.get('theme') === 'toggle';
 
-        if (shouldToggleTheme) {
-            const currentTheme = cookies.get('theme')?.value ?? DEFAULT_THEME;
+    if (shouldToggleTheme) {
+        const currentTheme = cookies.get('theme')?.value ?? DEFAULT_THEME;
 
-            const newTheme =
-                currentTheme === THEME.DARK ? THEME.LIGHT : THEME.DARK;
+        const newTheme = currentTheme === THEME.DARK ? THEME.LIGHT : THEME.DARK;
 
-            cookies.set('theme', newTheme);
+        cookies.set('theme', newTheme);
 
-            url.searchParams.delete('theme');
+        url.searchParams.delete('theme');
 
-            return Response.redirect(url);
-        }
+        return Response.redirect(url);
+    }
 
-        return await next();
-    },
-);
+    return next();
+});
