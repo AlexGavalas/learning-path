2872f3342cb47d31e0ee5dfc0952cf16d71768fa
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-01-26 00:27:48 +0200

    content: updates notes

--- scripts/publish-notes.ts
+++ scripts/publish-notes.ts
@@ -1,13 +1,26 @@
+import ora from 'ora';
 import { type SimpleGit, simpleGit } from 'simple-git';
 
 const main = async (): Promise<void> => {
     const git: SimpleGit = simpleGit({});
 
+    const spinner = ora('Adding content').start();
+
     await git.add('src/content/notes');
+
+    spinner.succeed('Added content');
+    spinner.text = 'Committing content';
+    spinner.start();
 
     await git.commit('content: updates notes');
 
+    spinner.succeed('Committed content');
+    spinner.text = 'Pushing content';
+    spinner.start();
+
     await git.push();
+
+    spinner.succeed('Pushed content');
 };
 
 main().catch(console.error);
