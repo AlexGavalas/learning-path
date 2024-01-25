82f8ffc9c9ed2cc62d3d9835e43918e2c0fcd0c7
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-01-25 23:45:08 +0200

    chore: updates packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ const colors = require('tailwindcss/colors');
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     darkMode: 'class',
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
     theme: {
         extend: {
             animation: {
