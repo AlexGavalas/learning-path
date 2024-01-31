4c616b67e9768342eb333c50dcabdbc8b6fc4b77
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-02-01 00:43:58 +0200

    chore: adds error message on env checks

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,15 @@ import invariant from 'tiny-invariant';
 
 import type { Database } from '~types/database.types';
 
-invariant(typeof process.env.PUBLIC_SUPABASE_URL === 'string');
-invariant(typeof process.env.PUBLIC_SUPABASE_ANON_KEY === 'string');
+invariant(
+    typeof process.env.PUBLIC_SUPABASE_URL === 'string',
+    'No PUBLIC_SUPABASE_URL found',
+);
+
+invariant(
+    typeof process.env.PUBLIC_SUPABASE_ANON_KEY === 'string',
+    'No PUBLIC_SUPABASE_ANON_KEY found',
+);
 
 export const supabase = createClient<Database>(
     process.env.PUBLIC_SUPABASE_URL,
