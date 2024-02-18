bdc46f56840f3b2176f790f54e37fd6d30f25751
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-02-18 15:42:47 +0200

    chore: moves db file

--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 
 import { logger } from '../../scripts/logger';
 
-const dbPath = path.join(process.cwd(), 'notes-db.sqlite3');
+const dbPath = path.join(process.cwd(), 'database', 'notes-db.sqlite3');
 
 export const db = new Database(dbPath, {
     verbose: logger.debug,
