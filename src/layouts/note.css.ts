6b8552d899e47ea58d7b341a7161706ecf04bb7b
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-07-12 22:31:08 +0300

    chore: updates dependencies

--- src/layouts/note.css.ts
+++ src/layouts/note.css.ts
@@ -37,6 +37,7 @@ globalStyle(`${note} :where(h1, h2, h3, h4, h5, h6)`, {
     color: vars.color.primary,
     fontWeight: 'bold',
     marginBottom: vars.spacing.lg,
+    textAlign: 'start',
 });
 
 globalStyle(`${note} p`, {
