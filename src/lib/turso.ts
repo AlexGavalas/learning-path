d1d4e2b3f0dc3f03abccb6cc6f15d403ca869d5c
Alex Gavalas <alex.gavalas.92@gmail.com>
2025-05-24 17:03:06 +0300

    refactor: removes tiny-invariant

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -1,15 +1,12 @@
 import { createClient } from '@libsql/client';
-import invariant from 'tiny-invariant';
 
-invariant(
-    typeof process.env.TURSO_DATABASE_URL === 'string',
-    'No TURSO_DATABASE_URL found',
-);
+if (typeof process.env.TURSO_DATABASE_URL !== 'string') {
+    throw new Error('No TURSO_DATABASE_URL found');
+}
 
-invariant(
-    typeof process.env.TURSO_AUTH_TOKEN === 'string',
-    'No TURSO_AUTH_TOKEN found',
-);
+if (typeof process.env.TURSO_AUTH_TOKEN !== 'string') {
+    throw new Error('No TURSO_AUTH_TOKEN found');
+}
 
 export const turso = createClient({
     authToken: process.env.TURSO_AUTH_TOKEN,
