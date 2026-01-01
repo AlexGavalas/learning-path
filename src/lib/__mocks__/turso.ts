5cd76f002e350d692085b93f1d96d5014cc78804
Alex Gavalas <alex.gavalas.92@gmail.com>
2026-01-01 23:16:57 +0100

    chore: updates dependencies

--- src/lib/__mocks__/turso.ts
+++ src/lib/__mocks__/turso.ts
@@ -1,10 +1,14 @@
-export const turso = {
+import { type Client } from '@libsql/client';
+
+export const turso: Client = {
     batch: vi.fn(),
     close: vi.fn(),
     closed: false,
     execute: vi.fn(),
     executeMultiple: vi.fn(),
+    migrate: vi.fn(),
     protocol: '',
+    reconnect: vi.fn(),
     sync: vi.fn(),
     transaction: vi.fn(),
 };
